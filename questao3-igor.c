613fbc513cd305e2f0e62f318f271fb2a3d9ff03
IgorLago <43051968+igorlago@users.noreply.github.com>
2018-11-29 17:33:14 -0200

    Update questao3-igor.c

--- questao3-igor.c
+++ questao3-igor.c
@@ -2,22 +2,23 @@
 #include<stdio.h>
 int main ()
 {
-	int objeto[3000], cont, mini, quan=1;
+	int objeto[5], cont, mini, quan=0;
 	
-	for(cont=0, cont<3000, cont++){
+	for(cont=0; cont<5; cont++){
 		printf("Coloque o peso do objeto %d em gramas:", cont+1);
-		scanf("%d", objeto[cont]);
+		scanf("%d", &objeto[cont]);
 	}
-	mini=objeto[0]
-	for (cont=1, cont<3000, cont++){
+	mini=objeto[0];
+	for (cont=0; cont<5; cont++){
 		if(objeto[cont]<mini){
 			mini=objeto[cont];
+			quan=0;
 		}
-		else if(objeto[cont]=mini){
+		else if(objeto[cont]==mini){
 			quan++;
 		}
 	}
-	printf("O menor peso e %d e tem %d objetos com esse peso.",mini, quant);
+	printf("O menor peso e %d e tem %d objetos com esse peso.",mini, quan);
 	
 	system("pause");
 	return 0;
