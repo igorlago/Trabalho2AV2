d017b177a7c898aba585ef3a32d036c161d956f0
IgorLago <43051968+igorlago@users.noreply.github.com>
2018-11-29 16:04:50 -0200

    Add files via upload

---
+++ questao2-igor.c
@@ -0,0 +1,23 @@
+//0050016037
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int cont, quan=0,tan;
+	char pa[30], let;
+	
+	printf("\nEntre com uma palavra:");
+	gets(pa);
+	tan=strlen(pa);
+	printf("\nEntre com uma letra:");
+	scanf("%c", &let);
+	
+	for(cont=0; cont<tan; cont++){
+		if(pa[cont]==let){
+			quan++;
+		}
+	}
+	printf("A letra %c apareceu %d vezes.",let ,quan);
+	system("pause");
+	return 0;
+}
